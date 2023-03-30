50bf9ae05d11de0e59efed8081a9858c31dd4d4b
Juan Carlos Hernandez Negrete <john.carlos.116@gmail.com>
2023-03-30 14:59:26 -0700

    Diseno principal actualizado + modificaciones a archivos

---
+++ projects/capacidades/vistas/modulos/load_slide.php
@@ -0,0 +1,4 @@
+ <!-- Pantalla de Carga Slide -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="vistas/assets/dist/img/AptivLogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
---
+++ projects/capacidades/vistas/modulos/navbar.php
@@ -0,0 +1,18 @@
+<!-- Barra de Navegacion OPCIONES -->
+  <nav class="main-header navbar navbar-expand navbar-black navbar-light">
+    <!-- NAVBAR para PHP !!! -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Menu Principal</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="vistas/filtrar.php" class="nav-link">Filtrar</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Simulacion</a>
+      </li>
+    </ul>
+  </nav>
